435c3bb9efe34b6213780ccf82ac28d8eb0cc050
Jose <diazjosec93@gmail.com>
2024-06-14 00:50:54 -0300

    database

--- estudiodiaz/settings.py
+++ estudiodiaz/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 from pathlib import Path
 import environ
@@ -83,11 +84,19 @@ WSGI_APPLICATION = 'estudiodiaz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
